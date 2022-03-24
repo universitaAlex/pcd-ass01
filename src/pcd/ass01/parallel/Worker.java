8e285b6d465f780eacecb74e8170dcbd12eaf6d9
alexbaiardi <alex.baiardi99@gmail.com>
2022-03-24 17:17:51 +0100

    Delete empty line in worker code

--- src/pcd/ass01/parallel/Worker.java
+++ src/pcd/ass01/parallel/Worker.java
@@ -26,50 +26,37 @@ public class Worker extends Thread {
 	}
 
 	public void run() {
-
 		/* simulation loop */
-
 		int iteration = 0;
 		while (!data.isOver()) {
 			iterationTracker.waitIteration(iteration);
 			/* update bodies velocity */
-
 			for (Body b : myBodies) {
-
 				/* compute total force on bodies */
 				V2d totalForce = computeTotalForceOnBody(b);
-
 				/* compute instant acceleration */
 				V2d acc = new V2d(totalForce).scalarMul(1.0 / b.getMass());
-
 				/* update velocity */
 				b.updateVelocity(acc, data.getDt());
 			}
-
 			try {
 				endForceComputationBarrier.hitAndWaitAll();
 			} catch (BrokenBarrierException e) {
 				e.printStackTrace();
 			}
-
 			/* compute bodies new pos and check collisions with boundaries */
-
 			for (Body b : myBodies) {
 				b.updatePos(data.getDt());
 				b.checkAndSolveBoundaryCollision(data.getBounds());
 			}
-
 			latch.countDown();
 			iteration++;
 		}
 	}
 
 	private V2d computeTotalForceOnBody(Body b) {
-
 		V2d totalForce = new V2d(0, 0);
-
 		/* compute total repulsive force */
-
 		for (int j = 0; j < data.getBodies().size(); j++) {
 			Body otherBody = data.getBodies().get(j);
 			if (!b.equals(otherBody)) {
@@ -80,10 +67,8 @@ public class Worker extends Thread {
 				}
 			}
 		}
-
 		/* add friction force */
 		totalForce.sum(b.getCurrentFrictionForce());
-
 		return totalForce;
 	}
 	
